8551b671604aa308f6c130cf10f2f1d3c25ba1e5
Mateusz Poliwczak <mpoliwczak34@gmail.com>
2024-10-18 19:57:07 +0200

    update

--- analyzer/analyzer_test.go
+++ analyzer/analyzer_test.go
@@ -21,56 +21,69 @@ func TestAnalyze(t *testing.T) {
 		t.Fatal(err)
 	}
 	for _, v := range files {
-		t.Run(v.Name(), func(t *testing.T) {
-			fileName := filepath.Join(testdata, v.Name())
-			c, err := os.ReadFile(fileName)
+		dirFiles := []string{v.Name()}
+		if v.IsDir() {
+			files, err := os.ReadDir(filepath.Join(testdata, v.Name()))
 			if err != nil {
 				t.Fatal(err)
 			}
-
-			tgo, errors, separatorFound := strings.Cut(string(c), "======\n")
-
-			fset := token.NewFileSet()
-			f, err := parser.ParseFile(fset, "test.tgo", tgo, parser.SkipObjectResolution|parser.ParseComments)
-			if err != nil {
-				t.Fatal(err)
+			dirFiles = []string{}
+			for _, f := range files {
+				dirFiles = append(dirFiles, filepath.Join(v.Name(), f.Name()))
 			}
-
-			ast.Print(fset, f)
-
-			var gotErrors strings.Builder
-			if err := Analyze(fset, f); err != nil {
-				for _, v := range err.(AnalyzeErrors) {
-					gotErrors.WriteString(v.Error())
-					gotErrors.WriteString("\n")
-				}
-				gotErrors.WriteString(err.Error())
-				gotErrors.WriteString("\n")
-			}
-
-			if *update {
-				out := tgo
-				if gotErrors.String() != "" {
-					out += "======\n" + gotErrors.String()
-				}
-				if err := os.WriteFile(fileName, []byte(out), 0666); err != nil {
+		}
+		for _, fileName := range dirFiles {
+			t.Run(fileName, func(t *testing.T) {
+				fileName := filepath.Join(testdata, fileName)
+				c, err := os.ReadFile(fileName)
+				if err != nil {
 					t.Fatal(err)
 				}
-				return
-			}
 
-			if !separatorFound {
-				if gotErrors.String() != "" {
-					t.Logf("source:\n%v", tgo)
-					t.Fatalf("unexpected errors, got:\n%v\nwant: <empty>", gotErrors.String())
+				tgo, errors, separatorFound := strings.Cut(string(c), "======\n")
+
+				fset := token.NewFileSet()
+				f, err := parser.ParseFile(fset, "test.tgo", tgo, parser.SkipObjectResolution|parser.ParseComments)
+				if err != nil {
+					t.Fatal(err)
 				}
-				return
-			}
 
-			if gotErrors.String() != errors {
-				t.Logf("source:\n%v", tgo)
-				t.Fatalf("unexpected errors, got:\n%v\nwant:\n%v", gotErrors.String(), errors)
-			}
-		})
+				ast.Print(fset, f)
+
+				var gotErrors strings.Builder
+				if err := Analyze(fset, f); err != nil {
+					for _, v := range err.(AnalyzeErrors) {
+						gotErrors.WriteString(v.Error())
+						gotErrors.WriteString("\n")
+					}
+					gotErrors.WriteString(err.Error())
+					gotErrors.WriteString("\n")
+				}
+
+				if *update {
+					out := tgo
+					if gotErrors.String() != "" {
+						out += "======\n" + gotErrors.String()
+					}
+					if err := os.WriteFile(fileName, []byte(out), 0666); err != nil {
+						t.Fatal(err)
+					}
+					return
+				}
+
+				if !separatorFound {
+					if gotErrors.String() != "" {
+						t.Logf("source:\n%v", tgo)
+						t.Fatalf("unexpected errors, got:\n%v\nwant: <empty>", gotErrors.String())
+					}
+					return
+				}
+
+				if gotErrors.String() != errors {
+					t.Logf("source:\n%v", tgo)
+					t.Fatalf("unexpected errors, got:\n%v\nwant:\n%v", gotErrors.String(), errors)
+				}
+			})
+		}
 	}
 }
