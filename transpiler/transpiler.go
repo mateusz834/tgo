dbe357334c4d2329860cee36293423ae7405dc84
Mateusz Poliwczak <mpoliwczak34@gmail.com>
2024-11-11 13:50:29 +0100

    update

--- transpiler/transpiler.go
+++ transpiler/transpiler.go
@@ -245,9 +245,9 @@ func (t *transpiler) tgoFunc(n ast.Node, funcType *ast.FuncType, body *ast.Block
 		params := funcType.Params
 		param := params.List[0]
 		if param.Names == nil {
-			t.appendFromSource(params.Opening + 1)
+			t.appendFromSource(param.Type.Pos())
 			t.appendSource("__tgo_ctx")
-			t.writeLineDirective(true, true, params.Opening+1)
+			t.writeLineDirective(true, true, param.Type.Pos())
 			t.appendFromSource(params.Closing)
 		} else if param.Names[0].Name == "_" {
 			t.appendFromSource(params.List[0].Names[0].Pos())
