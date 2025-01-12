b7c09c5ce4bcb842b0e121fa1340819a9bc7ff90
Mateusz Poliwczak <mpoliwczak34@gmail.com>
2025-01-12 19:48:44 +0100

    udpate

--- transpiler/transpiler.go
+++ transpiler/transpiler.go
@@ -1008,6 +1008,7 @@ func (t *transpiler) dynamicWriteIndent(x *ast.TemplateLiteralExpr, n *ast.Templ
 	lineDirectivePos := t.ctx.lastPosWritten
 
 	// TODO writeLineDirectiveSkipWhite?
+	// TODO: simplify
 	var prev iterWhiteResult
 	for v := range t.iterWhite(t.ctx.lastPosWritten, n.X.Pos()) {
 		if prev.whiteType != whiteInvalid {
