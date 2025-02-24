c991020fcad458cbe2633697d052d5de2ae4ea99
Mateusz Poliwczak <mpoliwczak34@gmail.com>
2025-02-24 09:57:28 +0100

    another assert

--- transpiler/transpiler.go
+++ transpiler/transpiler.go
@@ -728,6 +728,7 @@ func isTgo(n ast.Node, inTgoFunc bool) bool {
 	case *ast.ExprStmt:
 		x, isBasicLit := n.X.(*ast.BasicLit)
 		_, isTemplate := n.X.(*ast.TemplateLiteralExpr)
+		assert(isBasicLit || inTgoFunc)
 		return (isBasicLit && x.Kind == token.STRING && inTgoFunc) || isTemplate
 	}
 	return false
